b950117a13a03964952935d0d967f59165b3594c
Arian <arian>
2022-10-16 00:02:42 +0200

    limpio el codigo comentado

--- apps/TwitchBot-Consumer/src/Chat/Emotes/emotes.js
+++ apps/TwitchBot-Consumer/src/Chat/Emotes/emotes.js
@@ -4,25 +4,3 @@ const { ChannelEmoteHandler } = require('./ChannelEmoteHandler')
 module.exports = function (client){
     NewConsumerConnection('emotes',ChannelEmoteHandler,client);
 }
-// const Kafka = require('node-rdkafka');
-// const { client } = require('tmi.js');
-// const consumer = Kafka.KafkaConsumer({
-//     'group.id': 'kafka',
-//     'metadata.broker.list': 'localhost:9092'
-// }, {});
-// const { ChannelEmoteHandler } = require('./ChannelEmoteHandler')
-
-// module.exports = function (client) {
-//     consumer.connect();
-//     consumer.on('ready', function () {
-//         console.log('Emote consumer ready.') // we can start consuming messages
-//         consumer.subscribe(['emotes']) // subscribe to the topic 'emotes'
-//         consumer.consume() // start consuming messages
-//     }).on('data', (data) => {
-//         console.log(data)
-//         const message = JSON.parse(data.value);
-//         // deserialize the message
-//         ChannelEmoteHandler(message, client);
-//     });
-// }
-// TO DO Comprobar que el comando funcione bien ya que no esta funcionando correctamente

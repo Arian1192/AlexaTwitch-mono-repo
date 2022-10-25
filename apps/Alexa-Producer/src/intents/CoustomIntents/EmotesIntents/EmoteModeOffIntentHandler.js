2314d250e49dc7d6977e609f318e9257aac65244
Arian1192 <Arian.collaso.rodriguez@gmail.com>
2022-10-25 22:08:12 +0200

    Refactor FollowersModeOnIntent.js to use messageProducer

--- apps/Alexa-Producer/src/intents/CoustomIntents/EmotesIntents/EmoteModeOffIntentHandler.js
+++ apps/Alexa-Producer/src/intents/CoustomIntents/EmotesIntents/EmoteModeOffIntentHandler.js
@@ -5,15 +5,12 @@ const EmoteModeOffIntentHandler = {
         const { request } = handlerInput.requestEnvelope;
         return request.type === 'IntentRequest' && request.intent.name === 'EmoteModeOffIntent';
     },
-    handle(handlerInput)
+     handle(handlerInput)
     {
         messageProducer('emotes', 'emotes', 'emoteonlyoff')
         const speechText = 'Anfis por favor, desactiva el modo emotes.';
         return (
-            handlerInput.responseBuilder
-                .speak(speechText)
-                .reprompt('Hello world', speechText)
-                .getResponse()
+            handlerInput.responseBuilder.speak(speechText).reprompt('Hello world', speechText).getResponse()
         );
     }
 }
--- apps/Alexa-Producer/src/intents/CoustomIntents/FollowersModeIntent/FollowersModeOnIntent.js
+++ apps/Alexa-Producer/src/intents/CoustomIntents/FollowersModeIntent/FollowersModeOnIntent.js
@@ -1,28 +1,13 @@
-const { Kafka } = require('kafkajs');
-
-const kafka = new Kafka({
-    clientId: 'AlexaTwitch',
-    brokers: ['localhost:9092']
-});
-
-const producer = kafka.producer({groupId: 'followersmode'});
-
+const { messageProducer } = require("../../../../utils/messageProducer");
 const FollowersModeOnIntent = {
     canHandle(handlerInput)
     {
         const {request} = handlerInput.requestEnvelope;
         return request.type === 'IntentRequest' && request.intent.name === 'FollowersModeOnIntent';
     },
-    async handle(handlerInput)
+    handle(handlerInput)
     {
-        await producer.connect();
-        await producer.send({
-            topic: 'followers',
-            messages: [
-                { value: 'followersonly' },
-            ],
-        })
-        await producer.disconnect();
+        messageProducer('followersmode', 'followers', 'followersonly');
         const speechText = 'Anfis por favor, activa el modo de seguidores.';
         return (
             handlerInput.responseBuilder.speak(speechText).reprompt('Hello World').getResponse()
